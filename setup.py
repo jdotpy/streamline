ee569d7f9588449742bdf1595961d10ba27aab43
KJ <jdotpy@users.noreply.github.com>
2019-01-02 10:26:05 -0800

    Updated setup to include README

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from distutils.core import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name = 'streamline',
     scripts=['bin/streamline'],
     packages=['streamline'],
     version = '0.3.1',
     description = 'CLI tool for doing async tasks and transformations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = 'KJ',
     author_email = 'jdotpy@users.noreply.github.com',
     url = 'https://github.com/jdotpy/streamline',
     download_url = 'https://github.com/jdotpy/streamline/tarball/master',
     keywords = ['tools'],
-    classifiers = [],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
 )
